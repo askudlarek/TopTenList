ecaf367487d55412fb6989ef23daea96fbc60d9f
Adam Skudlarek <skudthe20@gmail.com>
2020-04-01 12:44:07 -0700

    Complete Wednesday Assignment

--- TopTenList.cpp
+++ TopTenList.cpp
@@ -15,24 +15,46 @@ TopTenList::TopTenList()
 
 void TopTenList::set_at(int index, Hyperlink link)
 {
-    _list.insert(_list.begin()+index-1, link);
+    _list[index-1] = link;
 }
 
 Hyperlink TopTenList::get(int index)
 {
-    return _list.at(index-1);
+    return _list[index-1];
 }
 
 
 void TopTenList::display_forward()
 {
-    for (int i = 0; i < 10; i++) {
-        cout << (i+1) + " " + _list[i].text << endl;
+    cout<<"Top Ten List Languages";
+    if( _list.size() == 0 )
+    {
+      cout<< " is empty.\n";
+    }
+    else
+    {
+      cout<< ":\n";
+    }
+
+    for (int i = 0; i < _list.size(); i++){
+        cout << i+1 << " of " << _list.size() << " ";
+        cout << _list[i].text << " " << _list[i].url << endl;
     }
 }
+
 void TopTenList::display_backward()
 {
-    for (int i = 9; i >= 0; i--) {
-        cout << (i) + " " + _list[i].text << endl;
+    cout<<"Top Ten List Languages";
+    if( _list.size() == 0 )
+    {
+      cout<< " is empty.\n";
     }
-}
+    else
+    {
+      cout<< ":\n";
+    }
+
+    for (int i = _list.size()-1; i >= 0; i--){
+        cout << i+1 << " of " << _list.size() << " ";
+        cout << _list[i].text << " " << _list[i].url << endl;
+    }}
